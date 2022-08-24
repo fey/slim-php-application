3749dd8af92117d4b6eabaa9bcaf8bf0e2769de1
feycot <feycot@gmail.com>
2022-08-25 02:00:45 +0500

    refactor config

---
+++ config/config.php
@@ -0,0 +1,33 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+use Illuminate\Support\Str;
+
+return [
+    'config.app.debug' => env('APP_DEBUG', false),
+    'config.app.env' => env('APP_ENV', 'development'),
+    'config.db.driver' => 'pgsql',
+    'config.db.host' => fn() => parse_url(env('DATABASE_URL', ''), PHP_URL_HOST) ?: env('DB_HOST'),
+    'config.db.port' => fn() => parse_url(env('DATABASE_URL', ''), PHP_URL_PORT) ?: env('DB_PORT', 5432),
+    'config.db.database' => fn() => trim(parse_url(env('DATABASE_URL', ''), PHP_URL_PATH), '/') ?: env('DB_DATABASE'),
+    'config.db.username' => fn() => parse_url(env('DATABASE_URL', ''), PHP_URL_USER) ?: env('DB_USERNAME'),
+    'config.db.password' => fn() => parse_url(env('DATABASE_URL', ''), PHP_URL_PASS) ?: env('DB_PASSWORD'),
+    'config.db.schema' => env('DB_SCHEMA', 'public'),
+    'config.db.connection' => function ($c): array {
+        return [
+            'driver' => $c->get('config.db.driver'),
+            'database' => $c->get('config.db.database'),
+            'username' => $c->get('config.db.username'),
+            'password' => $c->get('config.db.password'),
+            'port' => $c->get('config.db.port'),
+            'host' => $c->get('config.db.host'),
+        ];
+    },
+    'config.rollbar' => function (ContainerInterface $c): array {
+        return [
+            'environment' => $c->get('config.app.env'),
+            'access_token' => env('ROLLBAR_TOKEN', Str::random(32)),
+            'root' => dirname(__DIR__),
+        ];
+    },
+];
--- config/container.php
+++ config/container.php
@@ -29,53 +29,47 @@ $dotenv->safeLoad();
 
 $containerBuilder = new ContainerBuilder();
 
+$containerBuilder->addDefinitions(require __DIR__ . '/config.php');
 $containerBuilder->addDefinitions([
-    'app.debug' => env('APP_DEBUG', false),
-    'app.env' => env('APP_ENV', 'development'),
-    'db.driver' => 'pgsql',
-    'db.host' => fn() => parse_url(env('DATABASE_URL', ''), PHP_URL_HOST) ?: env('DB_HOST'),
-    'db.port' => fn() => parse_url(env('DATABASE_URL', ''), PHP_URL_PORT) ?: env('DB_PORT', 5432),
-    'db.database' => fn() => trim(parse_url(env('DATABASE_URL', ''), PHP_URL_PATH), '/') ?: env('DB_DATABASE'),
-    'db.username' => fn() => parse_url(env('DATABASE_URL', ''), PHP_URL_USER) ?: env('DB_USERNAME'),
-    'db.password' => fn() => parse_url(env('DATABASE_URL', ''), PHP_URL_PASS) ?: env('DB_PASSWORD'),
-    'db.schema' => env('DB_SCHEMA', 'public'),
-    'db.connection' => function (ContainerInterface $c): array {
-        return [
-            'driver' => $c->get('db.driver'),
-            'database' => $c->get('db.database'),
-            'username' => $c->get('db.username'),
-            'password' => $c->get('db.password'),
-            'port' => $c->get('db.port'),
-            'host' => $c->get('db.host'),
-            'fetch'     => \PDO::FETCH_ASSOC
-        ];
+    'app' => function (ContainerInterface $c): App {
+        $app = AppFactory::createFromContainer($c);
+
+        $app->addBodyParsingMiddleware();
+        $app->addRoutingMiddleware();
+
+        $app->add(function (Request $request, RequestHandler $handler): ResponseInterface {
+            /** @var \Psr\Log\LoggerInterface $logger */
+            $logger = $this->get('logger');
+            $logger->info('Request', [
+            'path' => $request->getUri()->getPath(),
+            'query_params' => $request->getQueryParams(),
+            'body' => $request->getParsedBody()
+            ]);
+
+            $response = $handler->handle($request);
+
+            $logger->info('Response', [
+            'status' => $response->getStatusCode(),
+            ]);
+
+            return $response;
+        });
+
+        $app->add(MethodOverrideMiddleware::class);
+        $app->addErrorMiddleware(true, true, true, $c->get('logger'));
+        $app->add(TwigMiddleware::createFromContainer($app));
+
+        return $app;
     },
-    'phoenix.config' => fn(ContainerInterface $c) => [
-        'migration_dirs' => [
-            __DIR__ . '/../database/migrations',
-        ],
-        'environments' => [
-            'pgsql' => [
-                'adapter' => 'pgsql',
-                'db_name' => $c->get('db.database'),
-                'host' => $c->get('db.host'),
-                'username' => $c->get('db.username'),
-                'password' => $c->get('db.password'),
-                'port' => $c->get('db.port'),
-            ],
-        ],
-        'default_environment' => 'pgsql',
-        'log_table_name' => 'phoenix_log',
-    ],
     'db' => function (ContainerInterface $c): Manager {
         $capsule = new Illuminate\Database\Capsule\Manager();
-        $connection = $c->get('db.connection');
+        $connection = $c->get('config.db.connection');
         $capsule->addConnection($connection);
         $capsule->setAsGlobal();
         $capsule->setEventDispatcher(new Dispatcher(new Container()));
         $capsule->bootEloquent();
 
-        if ($c->get('app.debug')) {
+        if ($c->get('config.app.debug')) {
             $capsule->connection()->listen(function ($query) use ($c) {
                 $c->get('logger')->debug($query->sql);
             });
@@ -95,7 +89,7 @@ $containerBuilder->addDefinitions([
 
         $twig->getEnvironment()->addGlobal('flash', $c->get('flash')->getMessages());
 
-        if ($c->get('app.debug')) {
+        if ($c->get('config.app.debug')) {
             $twig->getEnvironment()->enableDebug();
         }
 
@@ -104,61 +98,19 @@ $containerBuilder->addDefinitions([
     'flash' => function (): Messages {
         return new Messages();
     },
-    'rollbar.config' => function (ContainerInterface $c): array {
-        return [
-            'access_token' => env('ROLLBAR_TOKEN', Str::random(32)),
-            'environment' => $c->get('app.env'),
-            'root' => dirname(__DIR__),
-        ];
-    },
     'logger' => function (ContainerInterface $c): LoggerInterface {
         $logger = new Logger('name');
-        $logLevel = $c->get('app.debug') ? LogLevel::DEBUG : LogLevel::WARNING;
+        $logLevel = $c->get('config.app.debug')
+            ? LogLevel::DEBUG
+            : LogLevel::WARNING;
 
-        Rollbar::init($c->get('rollbar.config'));
+        Rollbar::init($c->get('config.rollbar'));
         $logger->pushHandler(new StreamHandler('php://stdout', $logLevel));
         $logger->pushHandler(new RollbarHandler(Rollbar::logger()), $logLevel);
         return $logger;
     },
-    'app' => function (ContainerInterface $c): App {
-        $app = AppFactory::createFromContainer($c);
-
-    // Parse json, form data and xml
-    $app->addBodyParsingMiddleware();
-
-    // Add the Slim built-in routing middleware
-    $app->addRoutingMiddleware();
-
-    $app->add(function (Request $request, RequestHandler $handler): ResponseInterface {
-        /** @var \Psr\Log\LoggerInterface $logger */
-        $logger = $this->get('logger');
-        $logger->info('Request', [
-            'path' => $request->getUri()->getPath(),
-            'query_params' => $request->getQueryParams(),
-            'body' => $request->getParsedBody()
-        ]);
-
-        $response = $handler->handle($request);
-
-        $logger->info('Response', [
-            'status' => $response->getStatusCode(),
-        ]);
-
-        return $response;
-    });
-
-    $app->add(MethodOverrideMiddleware::class);
-
-    // Handle exceptions
-    $app->addErrorMiddleware(true, true, true, $c->get('logger'));
-
-    $app->add(TwigMiddleware::createFromContainer($app));
-
-        return $app;
-    }
 ]);
 
-// Create DI container instance
 $container = $containerBuilder->build();
 
 return $container;
